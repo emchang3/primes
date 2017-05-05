5726053a9189d8874d952e388fc44a5846889d2f
Ezra M. Chang <ezra.chang@3dresults.com>
2017-05-04 23:52:19 -0500

    Added break statement for efficiency.

--- primes.js
+++ primes.js
@@ -1,7 +1,10 @@
 for (let i = 1; i < 101; i++) {
   let isPrime = true;
   for (let j = 1; j < i; j++) {
-    if (i % j === 0 && j !== 1 && j !== i) isPrime = false;
+    if (i % j === 0 && j !== 1 && j !== i) {
+      isPrime = false;
+      break;
+    }
   }
   if (isPrime === true) console.log(i);
 }
